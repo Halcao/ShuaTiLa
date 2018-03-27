e9399129792a2954df182d0f043ff2906c1477f6
Arsener <1769879278@qq.com>
2018-03-27 18:34:32 +0800

    添加bootstrap框架base_boot.html，需要解决模板与bootstrap兼容的问题

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from flask import render_template, redirect, url_for, flash, request, abort,current_app,session
+from flask import render_template, redirect, url_for, flash, request, abort, current_app, session
 from flask_login import current_user, login_required
 from . import auth
 from .. import config
@@ -10,13 +10,16 @@ import pymysql
 def index():
     # return render_template('index.html')
     return redirect(url_for('.auth_page'))
+
+
 @auth.route('/auth_page', methods=['GET', 'POST'])
 def auth_page():
     if session:
         name = session.get('username')
-        return render_template('auth.html',user_name=name)
+        return render_template('auth.html', user_name=name)
     else:
         return render_template('auth.html')
+
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
@@ -24,45 +27,46 @@ def login():
     password = request.values.get("logpass")
     db = pymysql.connect('localhost', 'root', config['MYSQL_PASSWORD'], 'net_lesson', charset='utf8')
     cur = db.cursor()
-    #sql =  "select Student_id, Student_password from student where Student_id='%s'"%(str(name))
-    cur.execute("select Student_id, Student_password from student where Student_id=%s",(name))
+    # sql =  "select Student_id, Student_password from student where Student_id='%s'"%(str(name))
+    cur.execute("select Student_id, Student_password from student where Student_id=%s", (name))
     results = cur.fetchall()
     cur.close()
     db.close()
-    if len(results)==0:
+    if len(results) == 0:
         flash(u"没有这个用户哦")
         return redirect(url_for('.auth_page'))
-    elif results[0][1]!=password:
-            # 需要提示密码错
+    elif results[0][1] != password:
+        # 需要提示密码错
         flash(u"密码输错了...")
         return redirect(url_for('.auth_page'))
     else:
-        session['username']= name
+        session['username'] = name
         return redirect(url_for('.auth_page'))
+
 
 @auth.route('/regist', methods=['GET', 'POST'])
 def regist():
     regname = request.values.get("regname")
-    regemail =request.values.get("regemail")
+    regemail = request.values.get("regemail")
     regpass = request.values.get("regpass")
     db = pymysql.connect('localhost', 'root', config['MYSQL_PASSWORD'], 'net_lesson', charset='utf8')
     cur = db.cursor()
-    cur.execute("select Student_id from student where Student_id=%s",(regname))
+    cur.execute("select Student_id from student where Student_id=%s", (regname))
     results = cur.fetchall()
-    if len(results)>0:
+    if len(results) > 0:
         flash(u"这个ID已经被注册了噢，换一个吧")
         return redirect(url_for('.auth_page'))
     else:
-        cur.execute("insert into student VALUES (%s,%s,%s)",(regname,regpass,regemail))
+        cur.execute("insert into student VALUES (%s,%s,%s)", (regname, regpass, regemail))
         flash(u"注册成功啦，现在可以登录啦")
     cur.close()
     db.commit()
     db.close()
     return redirect(url_for('.auth_page'))
 
-@auth.route('/log_off',methods=['GET','POST'])
+
+@auth.route('/log_off', methods=['GET', 'POST'])
 def log_off():
     if session:
         session.clear()
     return redirect(url_for('.auth_page'))
-
