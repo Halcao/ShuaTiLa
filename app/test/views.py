e4b949c68d69141e72b4092de23727d22d10d3ce
ssh <76481786@qq.com>
2018-04-04 21:49:45 +0800

    test收藏功能&BUG若干

--- app/test/views.py
+++ app/test/views.py
@@ -36,6 +36,33 @@ def test_page(id):
     sql_last = 'SELECT Last_index FROM lesson_info ' \
                'WHERE Lesson_id = %s AND Student_id = %s;'
 
+    sql_choice_like = 'SELECT Problem_id ' \
+                 'FROM choice_collection WHERE Lesson_id = %s AND Student_id=%s;'
+    sql_judge_like = 'SELECT Problem_id ' \
+                 'FROM judge_collection WHERE Lesson_id = %s AND Student_id=%s;'
+    sql_fill_like = 'SELECT Problem_id ' \
+                 'FROM fill_collection WHERE Lesson_id = %s AND Student_id=%s;'
+
+
+    cur.execute(sql_choice_like, (id,current_user.id))
+    questions = cur.fetchall()
+    all_like = []
+    for question in questions:
+        question1 = int(question[0]);
+        all_like.append(question1)
+
+    cur.execute(sql_judge_like, (id, current_user.id))
+    questions = cur.fetchall()
+    for question in questions:
+        question1 = int(question[0]);
+        all_like.append(question1)
+
+    cur.execute(sql_fill_like, (id, current_user.id))
+    questions = cur.fetchall()
+    for question in questions:
+        question1 = int(question[0]);
+        all_like.append(question1)
+
     cur.execute(sql_last, (id,S_id))
     last = cur.fetchone()
     if last is None:
@@ -74,10 +101,9 @@ def test_page(id):
         all_dict.append(dict(zip(question2, question1)))
     cur.close()
     db.close()
-
     return render_template('test.html', id=json.dumps(id), question_dict=json.dumps(question_dict),
                            judge_dict=json.dumps(judge_dict), fill_dict=json.dumps(fill_dict),
-                           all_dict=json.dumps(all_dict),last=json.dumps(last))
+                           all_dict=json.dumps(all_dict),last=json.dumps(last),all_like=json.dumps(all_like))
 
 
 @test.route('/save_page',methods=['GET', 'POST'])
@@ -86,14 +112,55 @@ def save_last():
     lessonid = request.form.get('lessonid')
     db = pymysql.connect('localhost', 'root', config['MYSQL_PASSWORD'], 'net_lesson', charset='utf8')
     cur=db.cursor()
-
     update_last = 'UPDATE lesson_info SET Last_index = %s WHERE Lesson_id = %s AND Student_id = %s'
-
-    test = 'SELECT Student_id FROM lesson_info'
     cur.execute(update_last, (userlast,lessonid,current_user.id))
     db.commit()
-    output = cur.fetchall()
-    #print "this is userlast "+ userlast
-    cur.close()
+    db.close()
+    return ''
+
+@test.route('/like_page',methods=['GET', 'POST'])
+def save_like():
+    likeid = request.form.get('likeid')
+    likeid=int(likeid)+1
+    lessonid = request.form.get('lessonid')
+    problemtype = int(request.form.get('problemtype'))
+    j_l=int(request.form.get('j_l'))
+    c_l = int(request.form.get('c_l'))
+    print "problemtype=",problemtype
+    db = pymysql.connect('localhost', 'root', config['MYSQL_PASSWORD'], 'net_lesson', charset='utf8')
+    cur = db.cursor()
+    if problemtype == 1:
+        update_like = 'INSERT INTO choice_collection VALUES (%s,%s,%s)'
+    if problemtype == 2:
+        likeid=likeid-c_l;
+        update_like = 'INSERT INTO judge_collection VALUES (%s,%s,%s)'
+    if problemtype == 3:
+        likeid = likeid - c_l-j_l;
+        update_like = 'INSERT INTO fill_collection VALUES (%s,%s,%s)'
+    cur.execute(update_like, (likeid, lessonid, current_user.id))
+    db.commit()
+    db.close()
+    return ''
+
+@test.route('/dislike_page',methods=['GET', 'POST'])
+def save_dislike():
+    dislikeid = request.form.get('dislikeid')
+    dislikeid = int(dislikeid) + 1
+    lessonid = request.form.get('lessonid')
+    problemtype = int(request.form.get('problemtype'))
+    j_l = int(request.form.get('j_l'))
+    c_l = int(request.form.get('c_l'))
+    db = pymysql.connect('localhost', 'root', config['MYSQL_PASSWORD'], 'net_lesson', charset='utf8')
+    cur = db.cursor()
+    if problemtype == 1:
+        update_dislike = 'DELETE FROM choice_collection WHERE Problem_id=%s AND Lesson_id=%s AND Student_id=%s'
+    if problemtype == 2:
+        dislikeid = dislikeid - c_l;
+        update_dislike = 'DELETE FROM judge_collection WHERE Problem_id=%s AND Lesson_id=%s AND Student_id=%s'
+    if problemtype == 3:
+        dislikeid = dislikeid - c_l - j_l;
+        update_dislike = 'DELETE FROM fill_collection WHERE Problem_id=%s AND Lesson_id=%s AND Student_id=%s'
+    cur.execute(update_dislike, (dislikeid, lessonid, current_user.id))
+    db.commit()
     db.close()
     return ''
