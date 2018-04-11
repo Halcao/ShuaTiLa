7fa8d0ceaa7074f88bdea264fa0e09a4cdfdb793
Arsener <1769879278@qq.com>
2018-04-11 16:16:46 +0800

    设置社区登录才可见

--- app/forum/views.py
+++ app/forum/views.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 from flask import redirect
+from flask_login import login_required
 from . import forum
 
 @forum.route('/', methods=['GET'])
+@login_required
 def index():
     return redirect('http://0.0.0.0:4567')
